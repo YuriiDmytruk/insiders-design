ec1c2cdc5d55811bb1bab58ea89b28023f0a1066
YuriiDmytruk <yuriydmytrukr@gmail.com>
2024-09-13 10:55:53 +0300

    add styles and assets

---
+++ styles.ts
@@ -0,0 +1,79 @@
+import {StyleSheet} from 'react-native';
+
+export const text = StyleSheet.create({
+  popins_700: {
+    fontFamily: 'popins',
+    fontWeight: 700,
+  },
+  popins_600: {
+    fontFamily: 'popins',
+    fontWeight: 600,
+  },
+  popins_500: {
+    fontFamily: 'popins',
+    fontWeight: 500,
+  },
+  popins_400: {
+    fontFamily: 'popins',
+    fontWeight: 400,
+  },
+  size_24_32:{
+    fontSize: 24,
+    lineHeight: 32,
+  },
+  size_20_28:{
+    fontSize: 20,
+    lineHeight: 28,
+  },
+  size_18_24:{
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  size_16_16:{
+    fontSize: 16,
+    lineHeight: 16,
+  },
+  size_14_20:{
+    fontSize: 14,
+    lineHeight: 20,
+  },
+  size_14_14:{
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  size_12_20:{
+    fontSize: 12,
+    lineHeight: 20,
+  },
+  size_12_16:{
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  textMainColor: {
+    color: '#121414',
+  },
+  textSecondaryColor: {
+    color: '#4F4F4F',
+  },
+  textRedColor: {
+    color: '#E43939',
+  },
+  textWhiteColor: {
+    color: '#FFFFFF',
+  },
+  textOrangeColor:{
+    color: '#EE7325',
+  },
+});
+
+export const colors = StyleSheet.create({
+    bgWhite: {
+        backgroundColor: '#FFFFFF',
+    },
+    bgOrange: {
+        backgroundColor: '#EE7325',
+    },
+    bgLightOrange: {
+        backgroundColor: '#FCF4ED',
+    },
+});
